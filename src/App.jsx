1b47e445589ea6bac2e273187bbb02f5dcaa2428
Nithishluhar <nithishluhar2606@gmail.com>
2024-01-04 09:49:52 +0530

    Disney+

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import "./App.css";
+import "./App.css";
 import GenreMovieList from "./Components/GenreMovieList";
 import Header from "./Components/Header";
 import ProdectionHoues from "./Components/ProdectionHoues";
